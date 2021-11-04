2b65b3220a0ea9ffccddfe69c493384ed06a3a80
Ryan Bethel <ryan.bethel@gmail.com>
2021-11-04 15:59:43 -0400

    initial code

---
+++ dist/.next/static/S2t5FcVFbKSY8pHAigzeT/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/621b40e2c451963c.css","static/chunks/pages/index-561c193a3b830737.js"],"/_error":["static/chunks/pages/_error-2280fa386d040b66.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
